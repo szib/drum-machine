9999bbf9e5155f8f5e5052b5825302310d9afc13
szib <szib77@gmail.com>
2019-03-20 09:42:41 +0000

    Plays sound on keypress

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -6,6 +6,21 @@ class DrumPad extends Component {
   constructor(props) {
     super(props);
     this.playSound = this.playSound.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress(e) {
+    if (this.props.keyCode === e.keyCode) {
+      this.playSound();
+    }
   }
 
   playSound(e) {
